9c9df4c5ed9257f2966458d148d7c6db28ea8cb1
Madfun2910 <Madfun2910@gmail.com>
2020-09-03 18:13:17 +0300

    First Commit
    First push to Git

---
+++ social-net-ts/src/componets/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://www.hotfootdesign.co.uk/wp-content/uploads/2016/10/apple.jpg" alt="IMG"/>
+        </header>
+    )
+}
+export default Header
---
+++ social-net-ts/src/componets/Profile.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://img2.akspic.ru/preview/151517-s_vysoty_ptichego_poleta-gorodskoj_pejzazh-delovoj_rajon-metropoliya-gollivud-550x310.jpg"
+                    alt=""/>
+            </div>
+
+            <div>
+                ava + discription
+            </div>
+
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2x</div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+export default Profile
